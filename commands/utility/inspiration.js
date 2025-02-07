962ac3b08221d69bf31ce0f1c0e20bb14244beef
Saunava-ghosh <gsaunava@gmail.com>
2025-02-07 15:38:36 +0530

    inspiration command

---
+++ commands/utility/inspiration.js
@@ -0,0 +1,33 @@
+async function inspireGen() {
+    try {
+        const baseUrl = 'http://api.forismatic.com/api/1.0/';
+        
+        const response = await fetch(`${baseUrl}?method=getQuote&format=json&lang=en`);
+        const data = await response.json();
+        const final_response = `"${data.quoteText}" - ${data.quoteAuthor || 'Anonymous'}`;
+        return final_response;
+    } catch (error) {
+        console.error('Error fetching quote:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    data: {
+        name: 'inspire',
+        description: 'Generates a random inspirational quote',
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+
+        try {
+            response = await inspireGen();
+        } catch (error) {
+            console.error(error);
+            response = 'There was an error processing your request.';
+        }
+
+        await interaction.reply(response);
+    }
+};
