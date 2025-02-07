0fdd07f22c2f15d1c20e0a5fb465ae0219e116de
Saunava-ghosh <gsaunava@gmail.com>
2025-02-07 16:05:11 +0530

    rizz command

---
+++ commands/utility/rizz.js
@@ -0,0 +1,31 @@
+async function pickupLineGen() {
+    try {
+        const response = await fetch("https://rizzapi.vercel.app/random");
+        const data = await response.json();
+        const pickup_line = data.text ;
+        return pickup_line;
+    } catch (error) {
+        console.error('Error fetching quote:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    data: {
+        name: 'rizz',
+        description: 'rizz someone by using a pickup line',
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+
+        try {
+            response = await pickupLineGen();
+        } catch (error) {
+            console.error(error);
+            response = 'There was an error processing your request.';
+        }
+
+        await interaction.reply(response);
+    }
+};
